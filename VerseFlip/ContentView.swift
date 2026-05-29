c5c67730af4e78d9173c3c7279574cc3dc7b76a4
John David Velos <velosjd092005@gmail.com>
2026-05-29 13:14:16 +0800

    Initial Commit

---
+++ VerseFlip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VerseFlip
+//
+//  Created by John David Velos on 5/29/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VerseFlip/VerseFlipApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerseFlipApp.swift
+//  VerseFlip
+//
+//  Created by John David Velos on 5/29/26.
+//
+
+import SwiftUI
+
+@main
+struct VerseFlipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
